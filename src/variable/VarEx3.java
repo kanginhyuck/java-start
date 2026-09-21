043443a1bcab24d13b48585616f45677168384fb
inhyuck <ihk0304@naver.com>
2026-09-21 23:31:16 +0900

    java study codes

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long a;
+        a = 10000000000L;
+        System.out.println(a);
+
+        boolean b;
+        b = true;
+        System.out.println(b);
+    }
+}
